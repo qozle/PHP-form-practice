a038df3eed6e7e041a19804f5728cd1313c6b328
Max <mf.qozle@gmail.com>
2018-10-04 18:12:03 -0400

    Experimenting
    Still just experimenting with mysqli.  Trying to get a feel for connecting to a database and making a table.

--- mysql_test.php
+++ mysql_test.php
@@ -23,37 +23,56 @@
 $servername = "localhost";
 $username = "qozle";
 $password = "193267abc";
-$dbname = "myDB"
+$dbname = "testFormDB";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password);
 
 // Check connection
 if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 }
 
-// sql to create table
-$sql = "CREATE TABLE FormInfo (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-firstname VARCHAR(30),
-lastname VARCHAR(30),
-email VARCHAR(30),
-website VARCHAR(30),
-comment VARCHAR(256)
-gender
+// SQL to create a database
+$sql = "CREATE DATABASE testFormDB";
+if ($conn->query($sql) === TRUE) {
+	echo "Database created successfully";
+} else {
+	echo "Error creating database: " . $conn->connect_error;
+}
 
+$conn->close();
 
+// Now connect to that database (did I have to kill the connection?  There's a better way to do this I'm pretty sure)
+$conn = new mysqli($servername, $username, $password, $dbname);
 
+// Check connection
+if ($conn->connect_error) {
+	die("Connection failed =( : " . $conn->connect_error);
+}
+
+// Now make some SQL to make a table...
+$sql = "CREATE TABLE SignUp (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+website VARCHAR(50),
+comment VARCHAR(256),
+gender SET('Male', 'Female', 'Other'),
+reg_date TIMESTAMP
 )";
 
-
-
+// Check that it works
+if ($conn->query($sql) === TRUE) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
 
 
 $conn->close();
 ?>
-
 
 </body>
 </html>
